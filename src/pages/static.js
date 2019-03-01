772e00994b6efdc8a7fff398a0d99adf4fbef2d1
Jevin Anderson <jevanderson@paypal.com>
2019-03-01 15:10:26 -0800

    Initial commit

---
+++ src/pages/static.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Static extends Component {
+  state = {};
+
+  // constructor(props) {}
+  // componentWillMount(){} // will be deprecated
+  // componentDidMount(){}
+  // componentWillReceiveProps(nextProps) {} // will be deprecated
+  // static getDerivedStateFromProps(nextProps, prevState) {}
+  // shouldComponentUpdate(nextProps, nextState) { return true; }
+  // componentWillUpdate(nextProps, nextState) {} // will be deprecated
+  // getSnapshotBeforeUpdate(prevProps, prevState) {}
+  // componentDidUpdate(prevProps, prevState, snapshot) {}
+  // componentWillUnmount() {}
+  // componentDidCatch(error, info) {}
+
+  render() {
+    return (
+      <div className="static">
+      </div>
+    );
+  }
+
+  static propTypes = {}
+
+  static defaultProps = {}
+}
+
+export default Static;
